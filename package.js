f7fc25e61c4219c222975a1cfa640e6aa46fed5e
Christian Klaussner <developer@klaussner.me>
2017-01-07 22:13:22 +0100

    Update `svelte:compiler` to 1.1.0

--- package.js
+++ package.js
@@ -1,18 +1,19 @@
 Package.describe({
   name: 'svelte:tracker',
-  version: '1.0.0',
+  version: '1.1.0',
   summary: 'Tracker integration for Svelte',
   git: 'https://github.com/meteor-svelte/svelte-tracker.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.4.2.3');
-
   api.use('tracker');
 
   // Compile ECMAScript and Svelte in this package
-  api.use('ecmascript');
-  api.use('svelte:compiler@1.0.0');
+  api.use([
+    'ecmascript',
+    'svelte:compiler@1.1.0'
+  ]);
 
   api.mainModule('with-tracker.js');
 });
