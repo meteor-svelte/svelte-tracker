922f61ffffd4e367f1bd102e4d869cfaa449df66
Christian Klaussner <christian@klaussner.me>
2018-05-07 11:06:17 +0200

    Use `svelte:compiler` 2.5.0_1 and bump version to 3.0.0

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'svelte:tracker',
-  version: '2.1.1',
+  version: '3.0.0',
   summary: 'Tracker integration for Svelte',
   git: 'https://github.com/meteor-svelte/svelte-tracker.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.4.2.3');
+  api.versionsFrom('1.6');
   api.use('tracker');
 
   // Compile ECMAScript and Svelte in this package.
   api.use([
     'ecmascript',
-    'svelte:compiler@1.40.1_1'
+    'svelte:compiler@2.5.0_1'
   ]);
 
   api.mainModule('with-tracker.js', [
